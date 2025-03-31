e9b6d7c038c1a7a12392c8f758d2bbc5ad75acd4
Lazernes <dbsauddn1@gmail.com>
2025-03-31 21:13:58 +0900

    [Init] initial setting

---
+++ src/test/java/com/example/seoulpublicdata2025backend/SeoulPublicData2025BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seoulpublicdata2025backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulPublicData2025BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
