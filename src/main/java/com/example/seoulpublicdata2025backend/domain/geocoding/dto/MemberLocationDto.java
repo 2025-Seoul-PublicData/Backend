994c81086884d1912f379b9a1f6089ef81cd87e8
Lazernes <dbsauddn1@gmail.com>
2025-04-16 13:49:40 +0900

    [Fix] DTO 유효성 검사 수정

--- src/main/java/com/example/seoulpublicdata2025backend/domain/geocoding/dto/MemberLocationDto.java
+++ src/main/java/com/example/seoulpublicdata2025backend/domain/geocoding/dto/MemberLocationDto.java
@@ -1,18 +1,24 @@
 package com.example.seoulpublicdata2025backend.domain.geocoding.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class MemberLocationDto {
-    
-    @NotBlank(message = "주소는 비어 있을 수 없습니다.")
-    private double latitude;
 
-    @NotNull(message = "not null")
-    private double longitude;
+    @NotNull(message = "위도는 null이 될 수 없습니다.")
+    @DecimalMin(value = "-90.0", inclusive = true, message = "위도는 -90 이상이어야 합니다.")
+    @DecimalMax(value = "90.0", inclusive = true, message = "위도는 90 이하이어야 합니다.")
+    private Double latitude;
 
-    @NotNull(message = "not null")
+    @NotNull(message = "경도는 null이 될 수 없습니다.")
+    @DecimalMin(value = "-180.0", inclusive = true, message = "경도는 -180 이상이어야 합니다.")
+    @DecimalMax(value = "180.0", inclusive = true, message = "경도는 180 이하이어야 합니다.")
+    private Double longitude;
+
+    @NotNull(message = "주소는 공백일 수 없습니다.")
     private String location;
 }
