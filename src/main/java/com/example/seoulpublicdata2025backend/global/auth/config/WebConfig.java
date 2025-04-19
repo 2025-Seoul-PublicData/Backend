bc4934de3b1b04221ad14f39ca98ecf1511338f9
sleepyhoon <pokjm99@naver.com>
2025-04-19 23:47:37 +0900

    [Feat] CORS URL 수정

--- src/main/java/com/example/seoulpublicdata2025backend/global/auth/config/WebConfig.java
+++ src/main/java/com/example/seoulpublicdata2025backend/global/auth/config/WebConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     // URL 확인 후에 수정할 수 있음
-    private static final String LOCAL_REACT_CLIENT_URL = "http://localhost:3000";
+    private static final String LOCAL_REACT_CLIENT_URL = "http://localhost:5173";
+    private static final String REACT_CLIENT_URL = "http://172.16.21.135:5173";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(LOCAL_REACT_CLIENT_URL)
+                .allowedOrigins(LOCAL_REACT_CLIENT_URL, REACT_CLIENT_URL)
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
