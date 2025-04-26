18953944dec3293178da72d9fceeaee4e4c6a47c
Lazernes <dbsauddn1@gmail.com>
2025-04-27 04:33:54 +0900

    [FEAT] Company 테이블 수정 및 ReviewCategory 수정

--- src/main/java/com/example/seoulpublicdata2025backend/domain/review/entity/ReviewCategory.java
+++ src/main/java/com/example/seoulpublicdata2025backend/domain/review/entity/ReviewCategory.java
@@ -3,10 +3,11 @@ package com.example.seoulpublicdata2025backend.domain.review.entity;
 public enum ReviewCategory {
 
     MEANINGFUL_CONSUMPTION, // 의미있는 소비였어요
-    KIND, // 친절해요
-    WANT_SUPPORT, // 응원하고 싶어요
+    ECO_FRIENDLY,   // 친환경적인 제품이에요
+    GOOD_QUALITY,   // 제품 품질이 좋아요
+    GOOD_SELECT, // 같이 사는 사회를 위한 좋은 선택 같아요
     HELP_OUR_AREA, // 우리지역에 도움이 돼요
     REASONABLE_PRICE, // 가격이 합리적이에요
     CLEAN, // 청결해요
-    COST, REVISIT // 다시 방문하고 싶어요
+    REVISIT // 다시 방문하고 싶어요
 }
