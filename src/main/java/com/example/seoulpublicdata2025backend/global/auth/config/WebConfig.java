76b7b8a2b267937c330b520962bd92b35d3872ba
sleepyhoon <pokjm99@gmail.com>
2025-04-27 00:18:52 +0900

    [Refactor] CORS 설정과 리다이렉트 주소를 프론트 배포 주소로 변경

--- src/main/java/com/example/seoulpublicdata2025backend/global/auth/config/WebConfig.java
+++ src/main/java/com/example/seoulpublicdata2025backend/global/auth/config/WebConfig.java
@@ -7,13 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     // URL 확인 후에 수정할 수 있음
-    private static final String LOCAL_REACT_CLIENT_URL = "http://localhost:5173";
-    private static final String REACT_CLIENT_URL = "http://172.16.21.135:5173";
+    private static final String REACT_CLIENT_URL = "https://morak.vercel.app";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(LOCAL_REACT_CLIENT_URL, REACT_CLIENT_URL)
+                .allowedOrigins(REACT_CLIENT_URL)
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
