895104dc43b5a73a77f21cca44c9eb7dd2643234
sleepyhoon <pokjm99@naver.com>
2025-04-28 16:18:46 +0900

    [Feat] CORS 주소 추가

--- src/main/java/com/example/seoulpublicdata2025backend/global/auth/config/WebConfig.java
+++ src/main/java/com/example/seoulpublicdata2025backend/global/auth/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.seoulpublicdata2025backend.global.auth.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     // URL 확인 후에 수정할 수 있음
-    private static final String REACT_CLIENT_URL = "https://morak.site";
+    private static final List<String> CLIENT_ORIGINS = List.of(
+            "https://morak.site",
+            "https://www.morak.site"
+    );
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(REACT_CLIENT_URL)
+                .allowedOrigins(CLIENT_ORIGINS.toArray(new String[0]))
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
