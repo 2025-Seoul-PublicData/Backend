8797fbd3b9b540dca942d843bbf1eceda67d4a34
Lazernes <dbsauddn1@gmail.com>
2025-05-04 22:14:23 +0900

    [Refactor] paymentInfoTime을 DateTimeFormat에서 jsonformat으로 수정합니다.

--- src/main/java/com/example/seoulpublicdata2025backend/domain/review/dto/CompanyReviewResponseDto.java
+++ src/main/java/com/example/seoulpublicdata2025backend/domain/review/dto/CompanyReviewResponseDto.java
@@ -1,9 +1,9 @@
 package com.example.seoulpublicdata2025backend.domain.review.dto;
 
 import com.example.seoulpublicdata2025backend.domain.review.entity.ReviewCategory;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.Set;
@@ -14,7 +14,7 @@ public class CompanyReviewResponseDto {
 
     private Long reviewId;
     private Long paymentInfoConfirmNum;
-    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
     private LocalDateTime paymentInfoTime;
     private Long companyId;
     private Long kakaoId;
