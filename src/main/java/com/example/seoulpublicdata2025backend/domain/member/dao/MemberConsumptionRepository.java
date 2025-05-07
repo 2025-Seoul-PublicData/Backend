50cd182ece024eca753b7060ed3e671f9b92e56e
sleepyhoon <pokjm99@naver.com>
2025-05-07 13:22:17 +0900

    [Fix] repository jpql @Param 추가

--- src/main/java/com/example/seoulpublicdata2025backend/domain/member/dao/MemberConsumptionRepository.java
+++ src/main/java/com/example/seoulpublicdata2025backend/domain/member/dao/MemberConsumptionRepository.java
@@ -8,12 +8,16 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberConsumptionRepository extends JpaRepository<MemberConsumption, Long> {
     Optional<MemberConsumption> findByMemberAndCompanyType(Member member, CompanyType companyType);
     @Query("SELECT m FROM MemberConsumption m WHERE m.member.kakaoId = :kakaoId")
-    List<MemberConsumption> findMemberConsumptionByKakaoId(Long kakaoId);
+    List<MemberConsumption> findMemberConsumptionByKakaoId(@Param("kakaoId") Long kakaoId);
 
     @Query("SELECT m FROM MemberConsumption m WHERE m.member.kakaoId = :kakaoId AND m.companyType = :companyType")
-    Optional<MemberConsumption> findConsumptionByKakaoIdAndCompanyType(Long memberId, CompanyType companyType);
+    Optional<MemberConsumption> findConsumptionByKakaoIdAndCompanyType(
+            @Param("kakaoId") Long kakaoId,
+            @Param("companyType") CompanyType companyType
+    );
 }
